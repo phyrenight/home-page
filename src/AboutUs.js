d9bca4d4faeff8e98dfa98797effe8a3ed515c41
Marc <phyrenight@gmail.com>
2017-07-24 23:55:21 -0400

    seperate footer and header from app class

---
+++ src/AboutUs.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+class AboutUs extends Component {
+	render() {
+	  return (
+	    <div> About Us</div>
+	  )
+	}
+}
+
+export default AboutUs
--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
+import AboutUs from './AboutUs.js';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div id="containsMain">
+        <Header />
+      <main>
+        <section className="mainSection">
+          <h2> Welcome to the home page</h2>
+        </section>
+        <AboutUs/>
+      </main>
+    <Footer/>
+    </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render(){
+    return(
       <header className="App">
         <div className="App-header">
           <h1>Heading</h1>
         </div>
         <nav role="navigation">
           <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Content</a></li>
-            <li><a href="">About Us</a></li>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/content">Content</Link></li>
+            <li><Link to="/AboutUs">About Us</Link></li>
           </ul>
         </nav>
       </header>
-      <main>
-        <section className="mainSection">
-          <h2> Welcome to the home page</h2>
-        </section>
-      </main>
+      )
+  }
+}
+
+
+class Footer extends Component{
+  render(){
+    return(
       <footer>
         <ul role="navigation">
           <li><a href="">Legal</a></li>
           <li><a href="">About Us</a></li>
         </ul>
       </footer>
-    </div>
-    );
+      )
   }
 }
 
